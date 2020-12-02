6b5e250eecd2835959f94f612e84a25c63553397
micat1996 <34137082+micat1996@users.noreply.github.com>
2020-12-03 04:54:20 +0900

    .

---
+++ Assets/Scripts/Components/AI/AIBehaviors/Enemy/AIBH_TrackingPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBH_TrackingPlayer : AIBehaviorBase
+{
+	[SerializeField] private float _MaxTrackingTime = 3.0f;
+	private EnemyCharacter _EnemyCharacter;
+	private PlayerCharacter _PlayerCharacter;
+
+	private WaitForSeconds _Wait05 = new WaitForSeconds(0.5f);
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		_EnemyCharacter = GetComponent<EnemyCharacter>();
+		_PlayerCharacter = PlayerManager.Instance.playerCharacter;
+	}
+
+	public override void Run()
+	{
+		IEnumerator Run()
+		{
+			float trackingStartTime = Time.time;
+
+			while (true)
+			{
+				if (Time.time - trackingStartTime > _MaxTrackingTime) break;
+
+				yield return _Wait05;
+				_EnemyCharacter.navMeshAgent.SetDestination(_PlayerCharacter.transform.position);
+			}
+
+			behaviourFinished = true;
+		}
+
+		StartCoroutine(Run());
+	}
+}
---
+++ Assets/Scripts/Components/Characters/EnemyCharacter/EnemyCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(BehaviorController))]
+public sealed class EnemyCharacter : MonoBehaviour
+{
+	public NavMeshAgent navMeshAgent { get; private set; }
+	public BehaviorController behaviorController { get; private set; }
+
+	private void Awake()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		behaviorController = GetComponent<BehaviorController>();
+	}
+
+	private void Update()
+	{
+		if (navMeshAgent.velocity.magnitude > 0.1f)
+		{
+			Vector3 xzDirection = navMeshAgent.velocity;
+			xzDirection.y = 0.0f;
+			xzDirection.Normalize();
+
+			LookAt(xzDirection);
+		}
+	}
+
+
+	public void LookAt(Vector3 lookDirecion)
+	{
+		transform.eulerAngles = lookDirecion.ToAngle() * Vector3.up;
+	}
+
+}
